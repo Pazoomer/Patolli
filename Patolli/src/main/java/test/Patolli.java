e8b19ac32a136dd25849116daed86e925a8e1de1
GitHElias <jorge.elias@potros.itson.edu.mx>
2024-10-09 11:19:29 -0700

    Documentacion test package

--- Patolli/src/main/java/test/Patolli.java
+++ Patolli/src/main/java/test/Patolli.java
@@ -3,13 +3,24 @@ package test;
 import presentation.FrameInicio;
 
 /**
- *
+ * Clase principal que inicia la aplicación Patolli.
+ * Esta clase configura la apariencia de la interfaz de usuario
+ * y muestra la ventana principal del juego.
+ * 
  * @author t1pas
  */
 public class Patolli {
 
+    /**
+     * Método principal que se ejecuta al iniciar la aplicación.
+     * Configura el look and feel de la interfaz de usuario y 
+     * crea y muestra la ventana de inicio.
+     *
+     * @param args los argumentos de línea de comandos (no se utilizan en esta aplicación)
+     */
     public static void main(String[] args) {
         try {
+            // Configura el look and feel a Nimbus si está disponible
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -17,8 +28,10 @@ public class Patolli {
                 }
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            // Registra cualquier excepción que se produzca al configurar el look and feel
             java.util.logging.Logger.getLogger(FrameInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        // Inicia la ventana de inicio en el hilo de eventos
         java.awt.EventQueue.invokeLater(() -> {
             new FrameInicio().setVisible(true);
         });
